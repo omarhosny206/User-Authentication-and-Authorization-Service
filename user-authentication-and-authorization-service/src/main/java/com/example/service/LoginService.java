8befb962f795cffa8f80f7f166e56fe42b867412
omarhosny206 <omarhosny102@gmail.com>
2022-08-13 18:35:42 +0200

    Added JWT dependency to pom.xml

--- user-authentication-and-authorization-service/src/main/java/com/example/service/LoginService.java
+++ user-authentication-and-authorization-service/src/main/java/com/example/service/LoginService.java
@@ -5,5 +5,5 @@ import com.example.model.LoginResponse;
 import org.springframework.web.bind.annotation.RequestBody;
 
 public interface LoginService {
-    public LoginResponse login(@RequestBody LoginRequest loginRequest) throws Exception;
+    public LoginResponse login(LoginRequest loginRequest) throws Exception;
 }
