0c0c52f89ea62547ce316de6b5c47bc3d2b2871d
omarhosny206 <omarhosny102@gmail.com>
2022-08-13 18:56:26 +0200

    Added SignupController for handling signup process for new users

---
+++ user-authentication-and-authorization-service/src/main/java/com/example/controller/SignupController.java
@@ -0,0 +1,24 @@
+package com.example.controller;
+
+import com.example.model.User;
+import com.example.service.SignupService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/signup")
+public class SignupController {
+    private final SignupService signupService;
+
+    public SignupController(SignupService signupService) {
+        this.signupService = signupService;
+    }
+
+    @PostMapping("/")
+    public ResponseEntity<String> signup(@RequestBody User user) {
+        return ResponseEntity.ok(signupService.signup(user));
+    }
+}
