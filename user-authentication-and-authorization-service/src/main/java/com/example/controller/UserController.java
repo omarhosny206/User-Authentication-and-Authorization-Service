9f635c1c607d33c57a17ad46961a035e7785ad28
omarhosny206 <omarhosny102@gmail.com>
2022-08-13 19:12:22 +0200

    Added UserController to have restricted endpoints based on roles

---
+++ user-authentication-and-authorization-service/src/main/java/com/example/controller/UserController.java
@@ -0,0 +1,47 @@
+package com.example.controller;
+
+import com.example.model.User;
+import com.example.service.UserService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Set;
+
+@RestController
+@RequestMapping("/users")
+@Slf4j
+public class UserController {
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/super-admin")
+    public ResponseEntity<List<User>> getAllAsSuperAdmin() {
+        log.info("Only user with role SUPER_ADMIN can consume this endpoint");
+        return ResponseEntity.ok().body(userService.getAll());
+    }
+
+    @GetMapping("/admin")
+    public ResponseEntity<List<User>> getAllAsAdmin() {
+        log.info("Only users with role [ADMIN, SUPER_ADMIN] can consume this endpoint");
+        return ResponseEntity.ok().body(userService.getAll());
+    }
+
+    @GetMapping("/manager")
+    public ResponseEntity<List<User>> getAllAsManager() {
+        log.info("Only users with role [MANAGER, ADMIN, SUPER_ADMIN] can consume this endpoint");
+        return ResponseEntity.ok().body(userService.getAll());
+    }
+
+    @GetMapping("/user")
+    public ResponseEntity<List<User>> getAllAsUser() {
+        log.info("Only users with role [USER, MANAGER, ADMIN, SUPER_ADMIN] can consume this endpoint");
+        return ResponseEntity.ok().body(userService.getAll());
+    }
+}
