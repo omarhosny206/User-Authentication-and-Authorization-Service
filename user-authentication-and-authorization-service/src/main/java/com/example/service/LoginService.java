645ef0c5b2b7c55f10ea2ddedc97eb873631d930
omarhosny206 <omarhosny102@gmail.com>
2022-08-13 18:33:44 +0200

    Added LoginService interface with login(loginRequest) method

---
+++ user-authentication-and-authorization-service/src/main/java/com/example/service/LoginService.java
@@ -0,0 +1,9 @@
+package com.example.service;
+
+import com.example.model.LoginRequest;
+import com.example.model.LoginResponse;
+import org.springframework.web.bind.annotation.RequestBody;
+
+public interface LoginService {
+    public LoginResponse login(@RequestBody LoginRequest loginRequest) throws Exception;
+}
