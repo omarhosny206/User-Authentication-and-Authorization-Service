e870da122ede8721eb269baa40a5e4de74fcb44a
omarhosny206 <omarhosny102@gmail.com>
2022-08-13 18:39:55 +0200

    Added LoginServiceImpl

---
+++ user-authentication-and-authorization-service/src/main/java/com/example/service/LoginServiceImpl.java
@@ -0,0 +1,48 @@
+package com.example.service;
+
+import com.example.model.LoginRequest;
+import com.example.model.LoginResponse;
+import com.example.util.JwtUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+public class LoginServiceImpl implements LoginService {
+    private final AuthenticationManager authenticationManager;
+    private final UserDetailsService userDetailsService;
+    private final JwtUtil jwtUtil;
+
+    public LoginServiceImpl(AuthenticationManager authenticationManager, UserDetailsService userDetailsService, JwtUtil jwtUtil) {
+        this.authenticationManager = authenticationManager;
+        this.userDetailsService = userDetailsService;
+        this.jwtUtil = jwtUtil;
+    }
+
+    @Override
+    public LoginResponse login(LoginRequest loginRequest) throws Exception {
+        log.info("making log in for user with username={}", loginRequest.getUserName());
+        String userName = loginRequest.getUserName();
+        String password = loginRequest.getPassword();
+
+        try {
+            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(
+                    userName,
+                    password
+            ));
+        } catch (BadCredentialsException e) {
+            throw new Exception("Incorrect username or password", e);
+        }
+
+        log.info("authenticating and generating JWT for user with username={}", loginRequest.getUserName());
+        UserDetails userDetails = userDetailsService.loadUserByUsername(userName);
+        String jwt = jwtUtil.generateToken(userDetails);
+
+        return new LoginResponse(jwt);
+    }
+}
