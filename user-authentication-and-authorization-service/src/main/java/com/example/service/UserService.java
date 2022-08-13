0209c5825bf6b5aaf608e4f2c92d59867caed3ee
omarhosny206 <omarhosny102@gmail.com>
2022-08-13 18:30:01 +0200

    Added UserService interface

---
+++ user-authentication-and-authorization-service/src/main/java/com/example/service/UserService.java
@@ -0,0 +1,11 @@
+package com.example.service;
+
+import com.example.model.User;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import java.util.List;
+
+public interface UserService {
+    public List<User> getAll();
+    public User getByUserName(String userName) throws UsernameNotFoundException;
+}
