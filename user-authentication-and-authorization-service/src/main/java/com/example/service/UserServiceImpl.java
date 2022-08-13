ea6401b1fb25b918426a259490eb0fccdf561fa8
omarhosny206 <omarhosny102@gmail.com>
2022-08-13 18:32:20 +0200

    Added CustomUserDetails model and UserServiceImpl

---
+++ user-authentication-and-authorization-service/src/main/java/com/example/service/UserServiceImpl.java
@@ -0,0 +1,42 @@
+package com.example.service;
+
+import com.example.model.CustomUserDetails;
+import com.example.model.User;
+import com.example.repository.UserRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@Slf4j
+public class UserServiceImpl implements UserService, UserDetailsService {
+    private final UserRepository userRepository;
+
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        log.info("Loading user with username={}", username);
+        User user = getByUserName(username);
+        System.out.println(user);
+        return new CustomUserDetails(user);
+    }
+
+    @Override
+    public List<User> getAll() {
+        log.info("Getting all users");
+        return userRepository.findAll();
+    }
+
+    @Override
+    public User getByUserName(String userName) throws UsernameNotFoundException {
+        log.info("Getting a user with username={}", userName);
+        return userRepository.findByUserName(userName);
+    }
+}
