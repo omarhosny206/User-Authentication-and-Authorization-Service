8e35af799162f405c980fcc53b89361019058d69
omarhosny206 <omarhosny102@gmail.com>
2022-08-13 18:25:34 +0200

    Added 2 models LoginRequest and LoginResponse

---
+++ user-authentication-and-authorization-service/src/main/java/com/example/model/LoginRequest.java
@@ -0,0 +1,13 @@
+package com.example.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class LoginRequest {
+    private String userName;
+    private String password;
+}
