826c50cab93714fc9ef212f48f3604eaec5cf86f
omarhosny206 <omarhosny102@gmail.com>
2022-08-13 18:57:48 +0200

    Added LoginController to authenticate the user and send back JWT

---
+++ user-authentication-and-authorization-service/src/main/java/com/example/controller/LoginController.java
@@ -0,0 +1,25 @@
+package com.example.controller;
+
+import com.example.model.LoginRequest;
+import com.example.model.LoginResponse;
+import com.example.service.LoginService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/login")
+public class LoginController {
+    private final LoginService loginService;
+
+    public LoginController(LoginService loginService) {
+        this.loginService = loginService;
+    }
+
+    @PostMapping("/")
+    public ResponseEntity<LoginResponse> login(@RequestBody LoginRequest loginRequest) throws Exception {
+        return ResponseEntity.ok(loginService.login(loginRequest));
+    }
+}
