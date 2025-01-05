0103e22a7d8493672a0cd004ca24685fb133bf44
Omar Hosny Keshk <omarhosny102@gmail.com>
2025-01-06 01:18:06 +0200

    Refactor

--- src/main/resources/db/migration/V2_0_0__create_user_table.sql
+++ src/main/resources/db/migration/V2_0_0__create_user_table.sql
@@ -1,11 +1,11 @@
-CREATE TABLE `user`
+CREATE TABLE "user"
 (
     id         BIGSERIAL PRIMARY KEY,
     first_name VARCHAR(255) NOT NULL,
     last_name  VARCHAR(255) NOT NULL,
-    username      VARCHAR(255) NOT NULL UNIQUE,
+    username   VARCHAR(255) NOT NULL UNIQUE,
     email      VARCHAR(255) NOT NULL UNIQUE,
     password   VARCHAR(255) NOT NULL,
     role_id    BIGINT       NOT NULL,
-    CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role (id) ON DELETE CASCADE
+    CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES "role" (id) ON DELETE CASCADE
 );
