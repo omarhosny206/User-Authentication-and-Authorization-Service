3b76b0467455e7a5be2e22e5b940fbbc8f6cf60c
Omar Hosny Keshk <omarhosny102@gmail.com>
2025-01-05 23:51:41 +0200

    Update folder structure

--- user-authentication-and-authorization-service/src/main/java/com/example/service/LoginService.java
+++
@@ -1,8 +0,0 @@
-package com.example.service;
-
-import com.example.dto.LoginRequest;
-import com.example.dto.LoginResponse;
-
-public interface LoginService {
-    public LoginResponse login(LoginRequest loginRequest);
-}
